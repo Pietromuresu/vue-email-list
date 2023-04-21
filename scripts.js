d08d7c93ec3d23dd169651d86676e1349c1085c4
Pietromuresu <muresupietro66@gmail.com>
2023-04-21 18:38:23 +0200

    display loading while filling arrMails

--- scripts.js
+++ scripts.js
@@ -4,21 +4,31 @@ createApp({
   data(){
     return {
       arrEmails: [],
-      email: ""
+      email: "",
+      loading: true
     }
   },
   methods: {
     getMail(){
+
+
       for(let i = 0; i < 10; i++){
 
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(result => {
           this.email = result.data.response
           this.arrEmails.push(this.email)
+          setTimeout(() =>{
+            this.loading = false},2000)
+          
         })
+       
+        
       }
+
     }
   },
   mounted(){
     this.getMail()
+  
   }
 }).mount("#app")

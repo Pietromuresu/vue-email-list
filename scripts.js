34832c8f6a6c5f37d4b0eb0a42fc85691fcf213b
Pietromuresu <muresupietro66@gmail.com>
2023-04-21 18:12:27 +0200

    create arrEmail and push 10 emails in it

--- scripts.js
+++ scripts.js
@@ -3,12 +3,22 @@ const {createApp} = Vue;
 createApp({
   data(){
     return {
+      arrEmails: [],
       email: ""
     }
   },
+  methods: {
+    getMail(){
+      for(let i = 0; i < 10; i++){
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(result => {
+          this.email = result.data.response
+          this.arrEmails.push(this.email)
+        })
+      }
+    }
+  },
   mounted(){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then(result => {
-      this.email = result.data.response
-    })
+    this.getMail()
   }
 }).mount("#app")
